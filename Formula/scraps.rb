25c18098cdc1f66c92b5f587e92ca63ccaf359a0
Taichi Kushiro <k.taichi0315@gmail.com>
2023-07-22 23:56:11 +0900

    Fix sha256

--- Formula/scraps.rb
+++ Formula/scraps.rb
@@ -2,7 +2,7 @@ class Scraps < Formula
   desc "A static site generator that builds a wiki from a set of markdown files. Inspired by https://scrapbox.io/."
   homepage "https://boykush.github.io/scraps"
   url "https://github.com/boykush/scraps/archive/refs/tags/v0.7.5.tar.gz"
-  sha256 "3e376f9ed3ac6c3569b0fa937d7f14596e4ffbd8"
+  sha256 "5773174fb32e6d64b1a62484f3573731848ee2c03d9b84a3d9b9f101c145cd89"
   license "MIT"
 
   depends_on "rust" => :build
