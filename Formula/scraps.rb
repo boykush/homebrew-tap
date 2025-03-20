319a57b0fb17db097f15550b2209a4efa613064e
Taichi Kushiro <23194090+boykush@users.noreply.github.com>
2025-03-21 00:19:56 +0900

    scraps 0.21.0
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/scraps.rb
+++ Formula/scraps.rb
@@ -1,8 +1,8 @@
 class Scraps < Formula
   desc "A static site generator that builds a wiki from a set of markdown files. Inspired by https://scrapbox.io/."
   homepage "https://boykush.github.io/scraps"
-  url "https://github.com/boykush/scraps/releases/download/v0.20.2/scraps-aarch64-apple-darwin.tar.gz"
-  sha256 "0c8d2f8bf81377e6853415c8eec34079899665d67e811ba591229991f3f98618"
+  url "https://github.com/boykush/scraps/releases/download/v0.21.0/scraps-aarch64-apple-darwin.tar.gz"
+  sha256 "9079c14c56b1091d5e6f99dc9f427c2bd005ad9a54f5118612b4e74fdad3b240"
   license "MIT"
 
   def install
